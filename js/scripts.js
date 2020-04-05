cf59f7e814e7c0bce3d938ea3199617a2441db02
Ivan Lebedzeu <ivan.lebedzeu@gmail.com>
2020-04-05 15:37:57 -0700

    fix business logick and console errors. Now depends on user input suggester will show all languages. If any conditions are met User will see an alert

--- js/scripts.js
+++ js/scripts.js
@@ -10,27 +10,42 @@ $(document).ready(function() {
     var newThings = $("input:radio[name=newThings]:checked").val();
     var jobForLife = $("input:radio[name=jobs]:checked").val();
 
-    if ((userGender === "Male" || userGender === "Female") && anyLanguages !== "No" && userCompany === "startup") {
+    
+    // if ((userGender === "Male" || userGender === "Female") && anyLanguages !== "No" && userCompany === "startup" && workField !== "Gaming" && newThings === "yes" && jobForLife === "wish" ) {
+    //   $("#kotlin").show();
+    // } else if (userGender !== "Bigender" && anyLanguages === "No" && userCompany === "corporate" && (workField === "Web" || workField === "Mobile") && newThings !== "not" && (jobForLife === "yes" || jobForLife === "wish" )) {
+    //   $("#java").show();
+    // } else if (userGender === "Bigender" && anyLanguages !== "more" && (userCompany === "startup" || userCompany === "corporate") && workField === "Gaming" && newThings === "not" && jobForLife === "usedTo") {
+    //   $("#python").show();
+    // } else if ((userGender !== "Male" && userGender !== "Female") && (anyLanguages === "more" || anyLanguages === "few") && (userCompany === "small" || userCompany === "startup") && workField !== "Enterprise" && (newThings === "timeToTime" || newThings === "stuck") && jobForLife !== "wish")  {
+    //   $("#ruby").show();
+    // }
+     if (userGender === "Male"){
       $("#kotlin").show();
-    } else if (userGender !== "Female" && (newThings === "yes" || newThings === "timeToTime") && jobForLife === "wish" && userCompany !== "corporate"){
+     } else if (userGender === "Female") {
       $("#java").show();
-    } else if ((newThings === "stuck" || newThings === "not" || newThings === "timeToTime") && workField !== "corporate"){
+     } else if (userGender === "Bigender"){
       $("#python").show();
-    } else if (jobForLife === "wish" && Gender !== "I prefere not to answer" && workField === "corporate") {
-     $("#ruby").show();
-    } else {
+     } else if (userGender === "I prefere not to answer") {
+      $("#ruby").show();
+     }
+
+      
+      
+      else {
       alert("We are really sorry but programming is not for you");
     }
+
+    
 
     $(".fullname").append(userName);
   
   });
+  
 
   $("#myBtn1").click(function(){
     $("#startQuiz").show();
     event.preventDefault(); 
-  });
-
-  
+  }); 
 
 });
